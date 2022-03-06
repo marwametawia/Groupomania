30e436b98bc9eb1888e1c156f85f9cc0fa855d12
marwametawia <metawia.marwa@live.fr>
2022-03-06 20:14:07 +0100

    correction

--- frontend/src/components/Feed/Comment.jsx
+++ frontend/src/components/Feed/Comment.jsx
@@ -10,10 +10,8 @@ export default function Comment({ postId }) {
     const [comment, setComment] = useState("");
     const createComment = useCreateComment();
     const deleteComment = useDeleteComment();
-    const {data:commentsData,isLoading} = useGetComments(postId, isOpen);
+    const { data: commentsData, isLoading } = useGetComments(postId, isOpen);
     const userData = useAuthenticatedUser();
-
-
 
     function handleChange(e) {
         setComment(e.target.value);
@@ -21,49 +19,58 @@ export default function Comment({ postId }) {
 
     return (
         <div className="comment">
-            <button onClick={() => setIsOpen(!isOpen) } >
-                { isOpen ? <span>Cacher les commentaires </span> : <span>Afficher les commentaires</span>}
+            <button onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? (
+                    <span>Cacher les commentaires </span>
+                ) : (
+                    <span>Afficher les commentaires</span>
+                )}
             </button>
-            {isOpen && (
-                isLoading ? (
+            {isOpen &&
+                (isLoading ? (
                     <span> Les commentaires se chargent </span>
                 ) : commentsData.length !== 0 ? (
                     commentsData.map((item) => (
-                        <div className="commentBox"> <div className="commentAuthor"key={item.id}>{item.user? item.user.firstName : "deleted user" }
-                        <div className="commentText" >
-                            {item.textContent}
+                        <div className="commentBox" key={item.id} >
+                            <div className="commentAuthor" >
+                                {item.user
+                                    ? item.user.firstName
+                                    : "deleted user"}
+                                <div className="commentText">
+                                    {item.textContent}
 
-                         { (userData.userId === item.userId || userData.isAdmin ) ? 
-                         <button onClick={()=>{
-                             
-                                deleteComment.mutate({postId, commentId:item.id})}}>
-                                Supprimer
-                            </button> : null }
-
+                                    {userData.userId === item.userId ||
+                                    userData.isAdmin ? (
+                                        <button
+                                            onClick={() => {
+                                                deleteComment.mutate({
+                                                    postId,
+                                                    commentId: item.id,
+                                                });
+                                            }}
+                                        >
+                                            Supprimer
+                                        </button>
+                                    ) : null}
+                                </div>
+                            </div>
                         </div>
-
-
-                         </div>
-                         </div>
-
-
-
                     ))
                 ) : (
                     <span>No comments</span>
-                )
-            )}
+                ))}
 
-            <div className="createComment" >
-                < input placeholder="écrire un commentaire"
-                        className="createComment"
-                        value={comment}
-                        onChange={handleChange}
+            <div className="createComment">
+                <input
+                    placeholder="écrire un commentaire"
+                    className="createComment"
+                    value={comment}
+                    onChange={handleChange}
                 />
 
                 <button
-                    onClick={()=>{
-                        createComment.mutate({postId, text: comment})
+                    onClick={() => {
+                        createComment.mutate({ postId, text: comment });
                     }}
                     className="createCommentButton"
                 >
--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import {Link} from "react-router-dom"
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
-import {toast, Toaster} from 'react-hot-toast';
+import {toast} from 'react-hot-toast';
 import {EmptyLayout} from '../layout/EmptyLayout';
 
 
